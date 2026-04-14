48598b84f728c0a78c90761d1048c7c4365d02c6
latoure <tukamothupi@gmail.com>
2026-04-14 13:19:06 +0200

    Firebase database setup

--- FireBase/firebase.js
+++ FireBase/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = app.firestore();
-const auth = firebase.auth();
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 export {analytics, db, auth }
